8b6425a8c556daac8d22ca3ba612b49f3845aaeb
Frank Kopp <frank@familie-kopp.de>
2018-01-03 12:00:25 +0100

    Added binding to model, keyboard handler, mouse handler

--- src/fko/breakout/controller/MainController.java
+++ src/fko/breakout/controller/MainController.java
@@ -26,8 +26,17 @@ package fko.breakout.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import fko.breakout.model.BreakOutModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 /**
  * MainController
  * 02.01.2018
@@ -35,20 +44,180 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
+	private BreakOutModel model;
+
 	/**
-	 * @param view
 	 * @param model
 	 */
-	public MainController() {
+	public MainController(BreakOutModel model) {
+		this.model = model;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
 	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+
 		System.out.println("HELLO from CONTROLLER");
+		
 	}
+
+	/**
+	 * @param model
+	 */
+	public void bindModelToView() {
+		// playfield
+		playfieldPane.prefWidthProperty().bind(model.playfieldWidthProperty());
+		playfieldPane.prefHeightProperty().bind(model.playfieldHeightProperty());
+		// paddle
+		System.out.println(model.paddleWidthProperty().get());
+		System.out.println(paddle.widthProperty().get());
+		paddle.widthProperty().bind(model.paddleWidthProperty());
+		paddle.heightProperty().bind(model.paddleHeightProperty());
+		
+		System.out.println(model.paddleXProperty().get()+":"+model.paddleYProperty().get());
+		System.out.println(paddle.xProperty().get()+":"+paddle.yProperty().get());
+		System.out.println(paddle.translateXProperty().get()+":"+paddle.translateYProperty().get());
+		System.out.println(paddle.layoutXProperty().get()+":"+paddle.layoutYProperty().get());
+		
+		paddle.layoutXProperty().bind(model.paddleXProperty());
+		paddle.layoutYProperty().bind(model.paddleYProperty());
+		
+//		// ball
+//		ball.radiusProperty().bind(model.ballRadiusProperty());
+//		ball.centerXProperty().bind(model.ballCenterXProperty());
+//		ball.centerYProperty().bind(model.ballCenterYProperty());
+		
+	}
+	
+	/**
+	 * @param b
+	 */
+	private void onPaddleLeftAction(boolean b) {
+		if (b) model.setPaddleLeft(true);
+		else model.setPaddleLeft(false);	
+	}
+	
+	/**
+	 * @param b
+	 */
+	private void onPaddleRightAction(boolean b) {
+		if (b) model.setPaddleRight(true);
+		else model.setPaddleRight(false);
+	}
+
+	/* ********************************************************
+	 * FXML 
+	 * ********************************************************/
+
+	@FXML
+	private Circle ball;
+
+	@FXML
+	private Button startStopButton;
+
+	@FXML
+	private Button pauseResumeButton;
+
+	@FXML
+	private Button soundButton;
+
+	@FXML
+	private Text levelLabel;
+
+	@FXML
+	private Text livesLabel;
+
+	@FXML
+	private Text pointsLabel;
+	
+	@FXML
+    private Pane playfieldPane;
+
+	@FXML
+	private Rectangle ceilingWall;
+
+	@FXML
+	private Rectangle leftWall;
+
+	@FXML
+	private Rectangle rightWall;
+
+	@FXML
+	private Rectangle paddle;
+
+	@FXML
+	void keyPressedAction(KeyEvent event) {
+		System.out.println("Key Pressed: "+event);
+		switch (event.getCode()) {
+		// game control
+		case SPACE: 		; break;
+		case ESCAPE:		; break;
+		case P: 			; break;
+		// paddle control
+		case LEFT:		onPaddleLeftAction(true); break;
+		case RIGHT:		onPaddleRightAction(true); break;
+		default:
+		}
+		
+	}
+
+	@FXML
+	void keyReleasedAction(KeyEvent event) {
+		System.out.println("Key Released: "+event);
+		switch (event.getCode()) {
+		// game control
+		case SPACE: 		; break;
+		case ESCAPE:		; break;
+		case P: 			; break;
+		// paddle control
+		case LEFT: 		onPaddleLeftAction(false); break;
+		case RIGHT:		onPaddleRightAction(false); break;
+		default:
+		}
+	}
+
+	@FXML
+	void mouseEnteredAction(MouseEvent event) {
+		//System.out.println("Mouse Entered: "+event);
+	}
+
+	@FXML
+	void mouseExitedAction(MouseEvent event) {
+		//System.out.println("Mouse Exited: "+event);
+	}
+
+	@FXML
+	void mouseMovedAction(MouseEvent event) {
+		model.setMouseXPosition(event.getX());
+	}
+
+	@FXML
+	void paddleMouseClickAction(MouseEvent event) {
+		System.out.println("Mouse Click: "+event);
+	}
+
+	@FXML
+	void paddleMouseReleasedAction(MouseEvent event) {
+		System.out.println("Mouse Release: "+event);
+	}
+
+	@FXML
+	void startStopButtonAction(ActionEvent event) {
+		System.out.println("Button StartStop: "+event);
+	}
+	
+	@FXML
+    void pauseResumeButtonAction(ActionEvent event) {
+		System.out.println("Button PauseResume: "+event);
+    }
+
+	@FXML
+	void soundButtonAction(ActionEvent event) {
+		System.out.println("Button Sound: "+event);
+	}
+
+
 
 }
