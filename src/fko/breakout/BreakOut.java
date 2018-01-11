34c88ab2aa2f67cc1f07c070d475de3c37416b62
Frank Kopp <frank@familie-kopp.de>
2018-01-11 14:06:16 +0100

    reformatting

--- src/fko/breakout/BreakOut.java
+++ src/fko/breakout/BreakOut.java
@@ -1,25 +1,25 @@
 /**
-MIT License
+ MIT License
 
-Copyright (c) 2018 Frank Kopp
+ Copyright (c) 2018 Frank Kopp
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
  */
 package fko.breakout;
 
@@ -37,125 +37,125 @@ import javafx.stage.Stage;
 /**
  * BreakOut / Arkanoid Clone in Java
  * <p>
- * 
+ *
  * <p>
  * 02.01.2018
  * @author Frank Kopp
- * 
+ *
  * TODO: use Logger
  */
 public class BreakOut extends Application {
 
-	// VERSION
-	public static final String VERSION = "0.3";
+  // VERSION
+  public static final String VERSION = "0.3";
 
-	private static Stage pStage;
+  private static Stage pStage;
 
-	/**
-	 * Main
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+  /**
+   * Main
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-	/**
-	 * @see javafx.application.Application#start(javafx.stage.Stage)
-	 */
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+  /**
+   * @see javafx.application.Application#start(javafx.stage.Stage)
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
 
-		pStage = primaryStage;
-		
-		BreakOutGame model = new BreakOutGame();
-		MainController controller = new MainController(model);
-		MainView view = new MainView(model, controller); 
+    pStage = primaryStage;
 
-		Scene scene = new Scene(view.asParent());
+    BreakOutGame model = new BreakOutGame();
+    MainController controller = new MainController(model);
+    MainView view = new MainView(model, controller);
 
-		primaryStage.setTitle("BreakOut Clone by Frank Kopp");
+    Scene scene = new Scene(view.asParent());
 
-		controller.bindModelToView(view);
+    primaryStage.setTitle("BreakOut Clone by Frank Kopp");
 
-		primaryStage.setScene(scene);
-		primaryStage.centerOnScreen();
-		primaryStage.setResizable(false);
+    controller.bindModelToView(view);
 
-		// closeAction
-		primaryStage.setOnCloseRequest(event -> {
-			Platform.exit();
-			System.exit(0);
-			event.consume();
-		});
+    primaryStage.setScene(scene);
+    primaryStage.centerOnScreen();
+    primaryStage.setResizable(false);
 
-		primaryStage.show();
-	}
+    // closeAction
+    primaryStage.setOnCloseRequest(event -> {
+      Platform.exit();
+      System.exit(0);
+      event.consume();
+    });
 
-	/**
-	 * @see javafx.application.Application#init()
-	 */
-	@Override
-	public void init() throws Exception {
-		super.init();
-		final URL urlResource = BreakOut.class.getResource("/fonts/AstronomicMono.otf");
-		if (urlResource == null) {
-			criticalError("Font could not be found!");
-		} else {
-			final String url = urlResource.toExternalForm();
-			if (Font.loadFont(url, 40) == null) {
-				criticalError("Font could not be loaded!");
-			}
-		}
-	}
+    primaryStage.show();
+  }
 
-	/**
-	 * Clean up and exit the application
-	 */
-	public static void exit() {
-		exit(0);
-	}
+  /**
+   * @see javafx.application.Application#init()
+   */
+  @Override
+  public void init() throws Exception {
+    super.init();
+    final URL urlResource = BreakOut.class.getResource("/fonts/AstronomicMono.otf");
+    if (urlResource == null) {
+      criticalError("Font could not be found!");
+    } else {
+      final String url = urlResource.toExternalForm();
+      if (Font.loadFont(url, 40) == null) {
+        criticalError("Font could not be loaded!");
+      }
+    }
+  }
 
-	/**
-	 * Clean up and exit the application
-	 */
-	private static void exit(int returnCode) {
-		Platform.exit();
-		System.exit(returnCode);
-	}
+  /**
+   * Clean up and exit the application
+   */
+  public static void exit() {
+    exit(0);
+  }
 
-	/**
-	 * Called when there is an unexpected unrecoverable error.<br>
-	 * Prints a stack trace together with a provided message.<br>
-	 * Terminates with <tt>exit(1)</tt>.
-	 * @param message to be displayed with the exception message
-	 */
-	public static void fatalError(String message, Object... args) {
-		Exception e = new Exception(String.format(message, args));
-		e.printStackTrace();
-		exit(1);
-	}
+  /**
+   * Clean up and exit the application
+   */
+  private static void exit(int returnCode) {
+    Platform.exit();
+    System.exit(returnCode);
+  }
 
-	/**
-	 * Called when there is an unexpected but recoverable error.<br>
-	 * Prints a stack trace together with a provided message.<br>
-	 * @param message to be displayed with the exception message
-	 */
-	public static void criticalError(String message, Object... args) {
-		Exception e = new Exception(String.format(message, args));
-		e.printStackTrace();
-	}
+  /**
+   * Called when there is an unexpected unrecoverable error.<br>
+   * Prints a stack trace together with a provided message.<br>
+   * Terminates with <tt>exit(1)</tt>.
+   * @param message to be displayed with the exception message
+   */
+  public static void fatalError(String message, Object... args) {
+    Exception e = new Exception(String.format(message, args));
+    e.printStackTrace();
+    exit(1);
+  }
 
-	/**
-	 * Called when there is an unexpected minor error.<br>
-	 * Prints a provided message.<br>
-	 * @param message to be displayed
-	 */
-	public static void minorError(String message,  Object... args) {
-		System.err.println(String.format(message, args));
-	}
+  /**
+   * Called when there is an unexpected but recoverable error.<br>
+   * Prints a stack trace together with a provided message.<br>
+   * @param message to be displayed with the exception message
+   */
+  public static void criticalError(String message, Object... args) {
+    Exception e = new Exception(String.format(message, args));
+    e.printStackTrace();
+  }
 
-	public static Stage getPrimaryStage() {
-		return pStage;
-	}
+  /**
+   * Called when there is an unexpected minor error.<br>
+   * Prints a provided message.<br>
+   * @param message to be displayed
+   */
+  public static void minorError(String message,  Object... args) {
+    System.err.println(String.format(message, args));
+  }
+
+  public static Stage getPrimaryStage() {
+    return pStage;
+  }
 
 
 }
